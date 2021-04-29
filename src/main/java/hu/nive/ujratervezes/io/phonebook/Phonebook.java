497ed47a7ed4e19f79dd61e712b3a0d2a12265a6
Straub Levente <slevy57@gmail.com>
2021-04-29 12:47:21 +0200

    one test is missing

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,31 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Phonebook {
+
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+
+
+        if (contacts == null) {
+            throw new IllegalArgumentException();
+        }
+        File file = new File(output);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue());
+                writer.newLine();
+            }
+            writer.flush();
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
